844f0945337ce018da62e22acca4f8c38b517353
Laurel-Jasper <laureljaspergit@gmail.com>
2021-12-26 09:55:08 -0800

    Add script to lap/reset button

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,38 @@
-const buttonSwitch = document.getElementById("multiple-button");
+const startStopSwitch = document.getElementById("start-stop-button");
+const lapResetSwitch = document.getElementById("lap-reset-button");
 // const stopButton = document.getElementById("stop-button");
 
-buttonSwitch.addEventListener("click", updateButton);
-
-function updateButton() {
-    if (buttonSwitch.textContent === "Start") {
-        buttonSwitch.textContent = "Stop";
-        buttonSwitch.style.color = "rgb(255, 47, 47)";
-        buttonSwitch.style.border = "rgb(255, 47, 47) solid 1px";
-        buttonSwitch.style.color = "rgb(255, 47, 47)";
-    } else if (buttonSwitch.textContent === "Stop") {
-        buttonSwitch.textContent = "Start";
-        buttonSwitch.style.color = "rgb(57, 106, 255)";
-        buttonSwitch.style.border = "rgb(57, 106, 255) solid 1px";
-    }
-    // buttonSwitch.style.display = 'none';
-    // stopButton.style.display = 'block';
+startStopSwitch.addEventListener("click", updateStartStop);
+// This button switches from start to stop
+function updateStartStop() {
+  if (startStopSwitch.textContent === "Start") {
+    startStopSwitch.textContent = "Stop";
+    startStopSwitch.style.color = "rgb(239, 239, 239)";
+    startStopSwitch.style.border = "rgb(255, 47, 47) solid 3px";
+    startStopSwitch.style.backgroundColor = "rgb(255, 47, 47)";
+  } else if (startStopSwitch.textContent === "Stop") {
+    startStopSwitch.textContent = "Start";
+    startStopSwitch.style.color = "rgb(239, 239, 239)";
+    startStopSwitch.style.border = "rgb(57, 106, 255) solid 3px";
+    startStopSwitch.style.backgroundColor = "rgb(57, 106, 255)";
+  }
+  // buttonSwitch.style.display = 'none';
+  // stopButton.style.display = 'block';
 }
+
+// function changes lap to reset
+
+startStopSwitch.addEventListener("click", updateLapResetButton);
+
+function updateLapResetButton() {
+  if (
+    lapResetSwitch.textContent === "Lap" &&
+    startStopSwitch.textContent === "Start"
+  ) {
+    lapResetSwitch.textContent = "Reset";
+  } else if (lapResetSwitch.textContent === "Reset" && startStopSwitch.textContent === "Stop") {
+    lapResetSwitch.textContent = "Lap";
+  } 
+}
+
+// left off at trying to figure out how to get the reset button to go back to the beginning
