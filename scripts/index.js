ba488547f010c9e78a18f6a71187458ee729f08c
Laurel-Jasper <laureljaspergit@gmail.com>
2021-12-31 17:03:36 -0800

    commented out most of project, to focus on basic stop watch

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,60 @@
-const startStopSwitch = document.getElementById("start-stop-button");
-const lapResetSwitch = document.getElementById("lap-reset-button");
-// const stopButton = document.getElementById("stop-button");
+// // BUTTONS
+// const startStopSwitch = document.getElementById("start-stop-button");
+// const lapResetSwitch = document.getElementById("lap-reset-button");
+// // const stopButton = document.getElementById("stop-button");
 
-startStopSwitch.addEventListener("click", updateStartStop);
-// This button switches from start to stop
-function updateStartStop() {
-  if (startStopSwitch.textContent === "Start") {
-    startStopSwitch.textContent = "Stop";
-    startStopSwitch.style.color = "rgb(239, 239, 239)";
-    startStopSwitch.style.border = "rgb(255, 47, 47) solid 3px";
-    startStopSwitch.style.backgroundColor = "rgb(255, 47, 47)";
-  } else if (startStopSwitch.textContent === "Stop") {
-    startStopSwitch.textContent = "Start";
-    startStopSwitch.style.color = "rgb(239, 239, 239)";
-    startStopSwitch.style.border = "rgb(57, 106, 255) solid 3px";
-    startStopSwitch.style.backgroundColor = "rgb(57, 106, 255)";
-  }
-  // buttonSwitch.style.display = 'none';
-  // stopButton.style.display = 'block';
-}
+// // WATCH
+// const watchOperations = document.getElementById("watch-display");
+// let seconds = 0;
 
-// function changes lap to reset
+// startStopSwitch.addEventListener("click", updateStartStop);
+// // This button switches from start to stop
+// function updateStartStop() {
+//   if (startStopSwitch.textContent === "Start") {
+//     startStopSwitch.textContent = "Stop";
+//     startStopSwitch.style.color = "rgb(239, 239, 239)";
+//     startStopSwitch.style.border = "rgb(255, 47, 47) solid 3px";
+//     startStopSwitch.style.backgroundColor = "rgb(255, 47, 47)";
+//   } else if (startStopSwitch.textContent === "Stop") {
+//     startStopSwitch.textContent = "Start";
+//     startStopSwitch.style.color = "rgb(239, 239, 239)";
+//     startStopSwitch.style.border = "rgb(57, 106, 255) solid 3px";
+//     startStopSwitch.style.backgroundColor = "rgb(57, 106, 255)";
+//   }
+//   // buttonSwitch.style.display = 'none';
+//   // stopButton.style.display = 'block';
+// }
 
-startStopSwitch.addEventListener("click", updateLapResetButton);
+// // function changes lap to reset
 
-function updateLapResetButton() {
-  if (
-    lapResetSwitch.textContent === "Lap" &&
-    startStopSwitch.textContent === "Start"
-  ) {
-    lapResetSwitch.textContent = "Reset";
-  } else if (lapResetSwitch.textContent === "Reset" && startStopSwitch.textContent === "Stop") {
-    lapResetSwitch.textContent = "Lap";
-  } 
-}
+// startStopSwitch.addEventListener("click", updateLapResetButton);
 
-// left off at trying to figure out how to get the reset button to go back to the beginning
+// function updateLapResetButton() {
+//   if (
+//     lapResetSwitch.textContent === "Lap" &&
+//     startStopSwitch.textContent === "Start"
+//   ) {
+//     lapResetSwitch.textContent = "Reset";
+//   } else if (lapResetSwitch.textContent === "Reset" && startStopSwitch.textContent === "Stop") {
+//     lapResetSwitch.textContent = "Lap";
+//   } 
+// }
+
+// // Change color to lap and reset to active
+// // startStopSwitch.addEventListener("click", updateLapAndResetToActive);
+// // function updateLapAndResetToActive () {
+// //   if (startStopSwitch.textContent === "Stop") {
+// //     updateLapAndResetToActive.style.color = "rgb(239, 239, 239)";
+// //     updateLapAndResetToActive.style.border = "rgb(255, 47, 47) solid 3px";
+// //     updateLapAndResetToActive.style.backgroundColor = "rgb(255, 47, 47)";
+// //   }
+// // }
+
+// // Watch Functions
+// startStopSwitch.addEventListener("click", watch);
+// function watch() { 
+//   if (startStopSwitch.textContent === "Stop") {
+    
+//   }
+// }
+
